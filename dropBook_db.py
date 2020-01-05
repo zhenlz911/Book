a3457870189f1244ac11905e0cb15af201953e17
zhenlz911 <zhenlz911@126.com>
2020-01-05 19:56:50 +0800

    数据更新

--- dropBook_db.py
+++ dropBook_db.py
@@ -4,36 +4,43 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from dropBookDialog import Ui_Dialog
 from db import PymysqlDb
+import datetime
 
 class DropBook(QDialog,Ui_Dialog):
-    def __init__(self):
-        super(DropBook,self).__init__()
+    drop_book_signal = pyqtSignal()
+    def __init__(self,parent = None):
+        super(DropBook,self).__init__(parent)
         self.setupUi(self)
+        self.setWindowModality(Qt.WindowModal)
         self.comobox()
         self.dropBookButton.clicked.connect(self.dropButt)
-        self.bookIdEdit.textChanged.connect(self.bookIdEditChanged)
+        self.bookIdEdit.editingFinished.connect(self.bookidchang)
 
     def comobox(self):
-        combox = ['科学', '政治', '经济', '哲学', '人文', '历史', '地理', '军事', '摄影', '财经']
+        combox = ['科学', '政治', '经济', '哲学', '人文', '历史', '地理', '军事', '摄影', '财经','']
         self.categoryComboBox.addItems(combox)
 
     #删除按钮
     def dropButt(self):
-        print('hello')
-
-    #书号改变后
-    def bookIdEditChanged(self):
-        bookid = self.bookIdEdit.text()
-        sql = "select * from book where BookId = '{}'".format(bookid)
-        row = PymysqlDb.query(sql)
-        if row:
-            self.bookNameEdit.setText(row[0])
-        return
+        pass
 
 
-
-
-
+    #输入书号后查询数据
+    def bookidchang(self):
+        bookid = self.bookIdEdit.text().strip()
+        if bookid != "":
+            sql = "select * from book where BookId = '%s'" % (bookid)
+            row = PymysqlDb().query(sql)
+            print(row)
+            if row:
+                self.bookNameEdit.setText(row[0][0])
+                self.authNameEdit.setText(row[0][2])
+                self.categoryComboBox.setCurrentText(row[0][3])
+                self.publisherEdit.setText(row[0][4])
+                #将时间类型转换成字符串型
+                self.publishTime.setText(datetime.datetime.strftime(row[0][8],'%Y-%m-%d'))
+            else:
+                print(QMessageBox.warning(self,'提示','没有查到此书信息,请核对',QMessageBox.Yes,QMessageBox.Yes))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
